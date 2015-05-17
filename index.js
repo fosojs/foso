750f6f38f938df633e82022ab1385ac1f01d5b1b
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-05-17 15:58:43 +0300

    Foso serve command implemented.

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+'use strict';
+
+
+var program = require('commander');
+var pkg = require('./package');
+var path = require('path');
+var serve = require('./lib/server');
+
+program
+  .version(pkg.version);
+
+program
+  .command('serve')
+  .usage('starts a Foso server in current directory')
+  .description('Starts a Foso server in current directory')
+  .action(function () {
+    var currentPath = path.resolve(process.cwd());
+    serve(currentPath);
+  });
+
+program.parse(process.argv);
