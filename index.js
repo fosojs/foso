bb12c743ebdb2d7c3e82ab5c6ef899727246f3ac
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-05-23 22:04:02 +0300

    Bundle command added.

--- index.js
+++ index.js
@@ -6,17 +6,37 @@ var program = require('commander');
 var pkg = require('./package');
 var path = require('path');
 var serve = require('./lib/server');
+var bundle = require('./lib/bundle');
 
 program
   .version(pkg.version);
 
 program
   .command('serve')
-  .usage('starts a Foso server in current directory')
+  .usage('[options]')
   .description('Starts a Foso server in current directory')
   .action(function() {
     var currentPath = path.resolve(process.cwd());
-    serve(currentPath);
+
+    serve(currentPath, {
+      buildFolder: getBuildFolder(currentPath)
+    });
   });
+
+program
+  .command('build')
+  .usage('[options]')
+  .description('Bundles Foso scripts')
+  .action(function() {
+    var currentPath = path.resolve(process.cwd());
+
+    bundle(currentPath, {
+      buildFolder: getBuildFolder(currentPath)
+    });
+  });
+
+function getBuildFolder(currentPath) {
+  return path.join(currentPath, './_build');
+}
 
 program.parse(process.argv);
